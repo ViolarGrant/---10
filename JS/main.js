18f768bc335e6de7eaaf55bde3fae451058d3c53
Valeriya Kroshnina <flora.valeriya@mail.ru>
2024-11-28 15:48:22 +0500

    Добавлены категории

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-const API_URL = "https://673864bf4eb22e24fca7c7b7.mockapi.io/products";
+const API_URL = "https://fakestoreapi.com/products";
 
 async function getProducts() {
   try {
@@ -20,30 +20,23 @@ function displayProducts(products) {
     productElement.className = "product";
     productElement.id = "prod";
     productElement.innerHTML = `
-      <img src="${product.img_product}" alt="${product.name_product}"></img>
-      <h3>${product.name_product}</h3>
-      <p>${product.desc_product}</p>
+      <img src="${product.image}" alt="${product.title}"></img>
+      <h3>${product.title}</h3>
+      <span>${product.description}</span>
+      <p>${product.category}</p>
       <button class="delete" data-id="${product.id}" id="delete">Удалить товар</button> 
     `;
-
-    /*const deleteBtn = productElement.querySelector(".delete");
-    deleteBtn.addEventListener("click", function () {
-      if (confirm("Вы точно хотите удалить этот продукт?")) {
-        productElement.remove();
-      }
-    });*/
-  
     const deleteBtn = productElement.querySelectorAll(".delete");
     deleteBtn.forEach((btn) => {
       btn.addEventListener("click", () => {
         fetch(
-          `https://673864bf4eb22e24fca7c7b7.mockapi.io/products/${btn.dataset.id}`,
+          `https://fakestoreapi.com/products/${btn.dataset.id}`,
           {
             method: "DELETE",
           }
         ).then(() => {
-          showMessage_2();
-          getProducts();
+          productElement.style.display = "none";
+          showMessage_2();          
         });
       });
     });
@@ -58,9 +51,10 @@ function displayProducts(products) {
 async function addProduct(event) {
   event.preventDefault();
   const newProduct = {
-    name_product: document.getElementById("productName").value,
-    img_product: document.getElementById("productImage").value,
-    desc_product: document.getElementById("productDescription").value,
+    title: document.getElementById("productName").value,
+    image: document.getElementById("productImage").value,
+    description: document.getElementById("productDescription").value,
+    category: document.getElementById("productCategory").value,
   };
   try {
     const response = await fetch(API_URL, {
@@ -69,11 +63,12 @@ async function addProduct(event) {
       body: JSON.stringify(newProduct),
     });
     if (!response.ok) throw new Error("Network response was not ok");
+    showMessage("Товар успешно создан");   
     getProducts();
-    showMessage("Товар успешно создан");
   } catch (error) {
     showMessage("Error adding products:" + error.message, "error");
   }
+  
 }
 
 function showMessage(message, type = "success") {
@@ -95,3 +90,28 @@ function showMessage_2(message, type = "success") {
 document.getElementById("product_form").addEventListener("submit", addProduct);
 
 getProducts();
+
+async function getCategories() {
+  try {
+    const response =  await fetch('https://fakestoreapi.com/products/categories');
+    if (!response.ok) throw new Error ("Network response was not ok");
+    const categories = await response.json();
+    filter(categories);
+  }  catch (error) {
+    showMessage("Error fetching products:" + error.message, "error");
+  }
+}
+
+function filter(categories){
+  const category = document.getElementById("filter");
+  category.textContent = categories;
+  category.className = "cat";
+}
+
+const filt = document.getElementById("filter");
+filt.addEventListener("click", getCategories);
+
+
+fetch('https://fakestoreapi.com/products/category/jewelery')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
