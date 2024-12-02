88841761cd2182e71819e7226d7b369d6faac7a4
Valeriya Kroshnina <flora.valeriya@mail.ru>
2024-12-02 16:21:43 +0500

    Попытка сделать сортировку

--- JS/main.js
+++ JS/main.js
@@ -29,24 +29,17 @@ function displayProducts(products) {
     const deleteBtn = productElement.querySelectorAll(".delete");
     deleteBtn.forEach((btn) => {
       btn.addEventListener("click", () => {
-        fetch(
-          `https://fakestoreapi.com/products/${btn.dataset.id}`,
-          {
-            method: "DELETE",
-          }
-        ).then(() => {
+        fetch(`https://fakestoreapi.com/products/${btn.dataset.id}`, {
+          method: "DELETE",
+        }).then(() => {
           productElement.style.display = "none";
-          showMessage_2();          
+          showMessage_2();
         });
       });
     });
     productlist.appendChild(productElement);
   });
 }
-
-
-
-
 
 async function addProduct(event) {
   event.preventDefault();
@@ -63,12 +56,11 @@ async function addProduct(event) {
       body: JSON.stringify(newProduct),
     });
     if (!response.ok) throw new Error("Network response was not ok");
-    showMessage("Товар успешно создан");   
+    showMessage("Товар успешно создан");
     getProducts();
   } catch (error) {
     showMessage("Error adding products:" + error.message, "error");
   }
-  
 }
 
 function showMessage(message, type = "success") {
@@ -81,7 +73,7 @@ function showMessage(message, type = "success") {
 
 function showMessage_2(message, type = "success") {
   const messageElement = document.getElementById("message_2");
-  messageElement.textContent = 'товар успешно удален';
+  messageElement.textContent = "товар успешно удален";
   messageElement.className = type;
   messageElement.style.display = "flex";
   setTimeout(() => (messageElement.style.display = "none"), 1000);
@@ -93,25 +85,57 @@ getProducts();
 
 async function getCategories() {
   try {
-    const response =  await fetch('https://fakestoreapi.com/products/categories');
-    if (!response.ok) throw new Error ("Network response was not ok");
+    const response = await fetch(
+      "https://fakestoreapi.com/products/categories"
+    );
+    if (!response.ok) throw new Error("Network response was not ok");
     const categories = await response.json();
-    filter(categories);
-  }  catch (error) {
+    displayCategories(categories);
+  } catch (error) {
     showMessage("Error fetching products:" + error.message, "error");
   }
 }
 
-function filter(categories){
-  const category = document.getElementById("filter");
-  category.textContent = categories;
-  category.className = "cat";
+function displayCategories(categories) {
+  const filterCat = document.getElementById("filterCat");
+  filterCat.innerHTML = "";
+
+  categories.forEach((categori) => {
+    const categoriItem = document.createElement("div");
+    categoriItem.className = "cat";
+    categoriItem.id = "catig";
+    categoriItem.innerHTML = `
+    <div>${categori}</div>
+    <button onclick=
+    "getCategoriesJewerly()" id="btnCat">click</button>
+    `;
+
+    filterCat.appendChild(categoriItem);
+  });
 }
 
 const filt = document.getElementById("filter");
 filt.addEventListener("click", getCategories);
 
+async function getCategoriesJewerly() {
+  try {
+    const response = await fetch(
+      'https://fakestoreapi.com/products/category/jewelery'
+    );
+    if (!response.ok) throw new Error("Network response was not ok");
+    const jewerly = await response.json();
+    console.log(jewerly);
+    displayJewerly(jewerly);
+  } catch (error) {
+    showMessage("Error fetching products:" + error.message, "error");
+  }
+}
 
-fetch('https://fakestoreapi.com/products/category/jewelery')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+function displayJewerly(jewerly) {
+  const product = document.getElementById("prod");
+  const jewerlyElem = document.getElementById("jewerly");
+  jewerlyElem.textContent = `${product}`;
+  jewerlyElem.style.display = "flex";
+}
+
+/*const jewerlyBtn = document.getElementById("btnCat").addEventListener("click", getCategoriesJewerly);*/
